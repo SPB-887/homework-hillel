80f9337b0960bda9f56531404060fae81f54d5c0
SPB-887 <makim1255@gmail.com>
2025-01-01 17:11:48 +0100

    Add files via upload

---
+++ js.1/script.js
@@ -0,0 +1,13 @@
+let counter =1;
+const number =7;
+
+while(10 >= counter) {
+    const result = number *counter;
+    console.log("7 *" +  counter + "=" + result);
+
+
+    counter =counter +1;
+}
+
+
+console.log("Ура Обчислення завершено!!!")
